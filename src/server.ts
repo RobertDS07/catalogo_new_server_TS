d8a498010d5574117e9d6baa7437f34d3cc84918
Robert Damaceno <robertdamacenobob@gmail.com>
2020-09-30 18:19:57 -0300

    :rocket: all configured, testing merges of types and resolvers... 1st contact with it

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import { graphqlHTTP } from 'express-graphql'
 
-import { schema, resolvers } from './graphql'
+import rootValue from './graphql/resolvers'
+import schema from './graphql/schemas'
 
 const app = express()
 
 app.use(cors())
 app.use('/graphql', graphqlHTTP({
     schema,
-    rootValue: resolvers,
+    rootValue,
     graphiql: true
 }))
 
